ec2361408c0bdf7cc2af4577f49eb222e9d33928
Unknown <pablo.osorio@xmarts.com>
2018-09-06 21:38:09 -0500

    Filtros para pagos y facturas del oehealth

--- models/models.py
+++ models/models.py
@@ -4,8 +4,28 @@ class OeHealthInherithPayment(models.Model):
     _inherit = 'account.payment'
     _description = 'Anexo a pagos'
 
-
+    hospital_origen = fields.Many2one('oeh.medical.health.center', string='Hospital Origen', default=lambda self: self.env.user.hospital_usuario)
     expediente = fields.Char(string='Expediente', compute="_get_expediente")
+
+    @api.model
+    def oeh_payments_searchs(self):
+        if self.env.user.id==1:
+            action = {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'tree,form',
+                'name': _('Pagos OeHealth'),
+                'res_model': 'account.payment',
+            }
+            return action
+        else:
+            action = {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'tree,form',
+                'name': _('Pagos OeHealth'),
+                'res_model': 'account.payment',
+                'domain': [('hospital_origen', '=', self.env.user.hospital_usuario.id)],
+            }
+            return action
 
     @api.one
     @api.depends('partner_id','expediente')
@@ -22,8 +42,28 @@ class OeHealthInherithPayment(models.Model):
     _inherit = 'account.invoice'
     _description = 'Anexo a facturas'
 
-
+    hospital_origen = fields.Many2one('oeh.medical.health.center', string='Hospital Origen', default=lambda self: self.env.user.hospital_usuario)
     expediente = fields.Char(string='Expediente', compute="_get_expediente")
+
+    @api.model
+    def oeh_invoices_searchs(self):
+        if self.env.user.id==1:
+            action = {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'tree,form',
+                'name': _('Facturas OeHealth'),
+                'res_model': 'account.invoice',
+            }
+            return action
+        else:
+            action = {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'tree,form',
+                'name': _('Facturas OeHealth'),
+                'res_model': 'account.invoice',
+                'domain': [('hospital_origen', '=', self.env.user.hospital_usuario.id)],
+            }
+            return action
 
     @api.one
     @api.depends('partner_id','expediente')
