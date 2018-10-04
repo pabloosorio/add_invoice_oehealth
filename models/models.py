fc0ac6e3da69c59f1b8d378cba0fd29205958230
Unknown <pablo.osorio@xmarts.com>
2018-10-04 14:03:45 -0500

    Add amount to text on payment and invoice

--- models/models.py
+++ models/models.py
@@ -1,11 +1,38 @@
 from odoo import api, fields, models, _
+from . import amount_to_text
 
 class OeHealthInherithPayment(models.Model):
     _inherit = 'account.payment'
     _description = 'Anexo a pagos'
 
-
+    hospital_origen = fields.Many2one('oeh.medical.health.center', string='Hospital Origen', default=lambda self: self.env.user.hospital_usuario)
     expediente = fields.Char(string='Expediente', compute="_get_expediente")
+    amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True, help='Amount of the invoice in letter')
+
+    @api.one
+    @api.depends('amount')
+    def _get_amount_to_text(self):
+        self.amount_to_text = amount_to_text.get_amount_to_text(self, self.amount, 'MXN') 
+
+    @api.model
+    def oeh_payments_searchs(self):
+        if self.env.user.id==1:
+            action = {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'tree,form',
+                'name': _('Pagos OeHealth'),
+                'res_model': 'account.payment',
+            }
+            return action
+        else:
+            action = {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'tree,form',
+                'name': _('Pagos OeHealth'),
+                'res_model': 'account.payment',
+                'domain': [('hospital_origen', '=', self.env.user.hospital_usuario.id)],
+            }
+            return action
 
     @api.one
     @api.depends('partner_id','expediente')
@@ -22,8 +49,34 @@ class OeHealthInherithPayment(models.Model):
     _inherit = 'account.invoice'
     _description = 'Anexo a facturas'
 
-
+    hospital_origen = fields.Many2one('oeh.medical.health.center', string='Hospital Origen', default=lambda self: self.env.user.hospital_usuario)
     expediente = fields.Char(string='Expediente', compute="_get_expediente")
+    amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True, help='Amount of the invoice in letter')
+
+    @api.one
+    @api.depends('amount_total')
+    def _get_amount_to_text(self):
+        self.amount_to_text = amount_to_text.get_amount_to_text(self, self.amount_total, 'MXN') 
+
+    @api.model
+    def oeh_invoices_searchs(self):
+        if self.env.user.id==1:
+            action = {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'tree,form',
+                'name': _('Facturas OeHealth'),
+                'res_model': 'account.invoice',
+            }
+            return action
+        else:
+            action = {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'tree,form',
+                'name': _('Facturas OeHealth'),
+                'res_model': 'account.invoice',
+                'domain': [('hospital_origen', '=', self.env.user.hospital_usuario.id)],
+            }
+            return action
 
     @api.one
     @api.depends('partner_id','expediente')
